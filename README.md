c309934b2bae0908f929d48bbf544ac09273b36e
Juan Mendez <juan.mendez1@grainger.com>
2022-04-26 18:40:18 -0500

    00: initial instructions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # jetpack-compose
+
+<img width="539" alt="image" src="https://user-images.githubusercontent.com/3371622/165410009-2bec8c4f-5abb-4ae6-9d7f-53936820330f.png">
--- app/src/main/java/com/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/jetpack/compose/MainActivity.kt
@@ -4,37 +4,60 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jetpack.compose.ui.theme.ComposeTheme
 
+/**
+ * In my case I rename code hint to refresh for control + r, I can now update previous state quicker :)
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /**
+         * You use setContent to define your layout, but instead of using an XML file as you'd do in the traditional View system,
+         * you call Composable functions within it.
+         */
         setContent {
-            ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            // right click, refactor, function to scope, and apply MainActivity.kt
+            // then name your method `MyApp`, and it will generate MyApp function below
+            MyApp()
         }
     }
 }
 
 @Composable
+private fun MyApp() {
+    ComposeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.primary) {
+            Greeting("Android")
+        }
+    }
+}
+
+/**
+ * A Compose app is made up of composable functions - just regular functions marked with @Composable, which can call other composable functions.
+ * A function is all you need to create a new UI component. The annotation tells Compose to add special support to the function for updating and
+ * maintaining your UI over time. Compose lets you structure your code into small chunks. Composable functions are often referred to as "composables"
+ * for short.
+ */
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeTheme {
-        Greeting("Android")
-    }
+    MyApp()
 }
