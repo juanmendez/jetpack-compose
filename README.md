8b563f0d4d63b555aac008abce84a1302beadfc9
Juan Mendez <juan.mendez1@grainger.com>
2022-04-30 13:52:07 -0500

    01: working with scaffolds

--- README.md
+++ README.md
@@ -4,8 +4,7 @@ Rather than modifying a widget in a decorated pattern, from the inside out. In J
 We have been accustomed when dealing with xml layouts to stylize buttons with a drawable to define background, corners, and strokes.
 We also do the same with adding text styles, and limited ways to add icons either at the start or end.
 
-Similarly with what Flutter does with wrapping widgets with other widgets, we are giving room within a composable to substitute 
-its content. 
+In Jetpack Compose we are giving room within a composable to substitute its content. Similarly with what Flutter does with wrapping widgets with other widgets.
 
 How we have been dealing with modifying a button
 ```kotlin
@@ -28,3 +27,8 @@ Button {
     }
 }
 ```
+
+Scaffold is a widget used also in Flutter. So its task is to create a material screen layout. In this demo we worked with adding a Material toolbar.
+And since we work with slots, we can substitute some of its content.
+
+<img width="423" alt="Screen Shot 2022-04-30 at 1 49 10 PM" src="https://user-images.githubusercontent.com/3371622/166118778-148f76c9-e606-4952-91da-6109659b1ed2.png">
--- app/src/main/java/com/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/jetpack/compose/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.jetpack.compose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jetpack.compose.ui.theme.LayoutsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,31 +25,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutsCodelabTheme {
-                LayoutsCodelab()
+                LayoutsCodelab(clickable = {
+                    Toast.makeText(baseContext, "Pour some sugar on me!!!!", Toast.LENGTH_LONG).show()
+                })
             }
         }
     }
 }
 
 @Composable
-fun LayoutsCodelab() {
+fun LayoutsCodelab(clickable: () -> Unit = {}) {
     Scaffold(
         topBar = {
-            Text(
-                text = "LayoutsCodelab",
-                style = MaterialTheme.typography.h3
+            TopAppBar(
+                title = {
+                    Text(text = "Def Leppard")
+                },
+                actions = {
+                    IconButton(onClick = clickable) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
             )
         }
     ) { innerPadding ->
         BodyContent(innerPadding)
     }
+
 }
 
 @Composable
 fun BodyContent(innerPadding: PaddingValues) {
     Column(modifier = Modifier.padding(innerPadding)) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+        Text(text = "Hysteria", modifier = Modifier.padding(vertical = 18.dp))
+        Text(text = "Hysteria is the fourth studio album by English rock band Def Leppard, released on 3 August 1987 through Mercury Records. It is Def Leppard's best-selling album to date, selling over 20 million copies worldwide, including 12 million in the US, and spawning seven hit singles. The album charted at number one on both the Billboard 200 and the UK Albums Chart.")
     }
 }
 
