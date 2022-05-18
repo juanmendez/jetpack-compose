3a382dbfc5469503eba1719fb111fd5d9abdfbb5
Juan Mendez <juan.mendez1@grainger.com>
2022-05-17 19:15:04 -0500

    02:01: rendering images in listviews.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Jetpack Compose - List View
 
+More about list views in Jetpack Compose.
  - Lists can also have their states to be remembered, specially when rotating device.
+ - In Jetpack Compose whenever we want to load up images we can use [Coil](https://github.com/coil-kt/coil) which works similarly like [Glide](https://github.com/bumptech/glide).
+ - Just like any other framework is good to know the lifecycle of composables, checkout this [guideline.](https://developer.android.com/jetpack/compose/lifecycle)
+
+<img width="422" alt="image" src="https://user-images.githubusercontent.com/3371622/167228666-fa25acd4-52b0-4bcb-bb3b-fe7ac22ee6d3.png">
--- app/src/main/java/com/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/jetpack/compose/MainActivity.kt
@@ -4,23 +4,33 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 import com.jetpack.compose.ui.theme.LayoutsCodelabTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,19 +68,59 @@ fun LayoutsCodelab(clickable: () -> Unit = {}) {
 
 @Composable
 fun BodyContent() {
-    ScrollableList(PaddingValues(30.dp))
+    ScrollableList()
 }
 
 @Composable
-fun ScrollableList(innerPadding: PaddingValues) {
-    // We save the scrolling position with this state that can also
-    // be used to programmatically scroll the list
-    val scrollState = rememberScrollState()
+fun ScrollableList() {
+    val listSize = 100
+    // We save the scrolling position with this state
+    val scrollState = rememberLazyListState()
 
-    Column(Modifier.verticalScroll(scrollState)) {
-        repeat(40) {
-            Text("Item #$it", Modifier.padding(innerPadding))
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Button(onClick = {
+            coroutineScope.launch {
+                // 0 is the first item index
+                scrollState.animateScrollToItem(0)
+            }
+        }, Modifier.fillMaxWidth()) {
+            Text("Scroll to the top")
         }
+        LazyColumn(state = scrollState, modifier = Modifier
+            .weight(1f, true)
+            .fillMaxWidth()) {
+            items(listSize) {
+                ImageListItem(it)
+            }
+        }
+
+        Button(onClick = {
+            coroutineScope.launch {
+                // listSize - 1 is the last index of the list
+                scrollState.animateScrollToItem(listSize - 1)
+            }
+        }, Modifier.fillMaxWidth()) {
+            Text("Scroll to the end")
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+
+        Image(
+            painter = rememberAsyncImagePainter(
+                model = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
     }
 }
 
