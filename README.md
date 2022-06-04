d3cf932c6e25af4d60f7fb73f4500c761197d071
Juan Mendez <juan.mendez1@grainger.com>
2022-06-04 14:54:27 -0500

    06: vertical grid

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# Jetpack Compose - Row arrangements
+# Jetpack Compose - Lazy Grids
+
+Lazy grid is only available currently in vertical flavor.
+The demo has horizontal, but that's no longer available. What we could have done instead was to use a lazy row which was populated by columns having two cells.
+
+What was in Codelabs
+
+```kotlin
+// Step: Favorite collections grid - LazyGrid
+@Composable
+fun FavoriteCollectionsGrid(
+    modifier: Modifier = Modifier
+) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.height(120.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollectionCard(item.drawable, item.text, Modifier.height(56.dp))
+        }
+    }
+}
+```
+
+What I did instead:
+
+```kotlin
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun FavoriteCollectionsGrid(
+    modifier: Modifier = Modifier
+) {
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ) {
+        items(favoriteCollectionsData) { item ->
+            AlignYourBodyElement(item.drawable, item.text)
+        }
+    }
+}
+```
+
+Here is the new vertical grid. Disregard Guns n' Roses copy. I needed to make sure the text was centered. :)
+
+<img width="427" alt="image" src="https://user-images.githubusercontent.com/3371622/172023212-88f889bb-4986-4a7d-a34c-c31ee09f2706.png">
+
+
+View from a Samsung S21
+
+<img width="427" alt="image" src="https://user-images.githubusercontent.com/3371622/172023290-cc09c30d-e06f-4682-883e-0672de267078.png">
--- app/src/main/java/com/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/jetpack/compose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -16,7 +17,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
@@ -34,6 +37,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jetpack.compose.ui.theme.ComposeTheme
@@ -66,12 +70,22 @@ private val alignYourBodyData = listOf(
     R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
 ).map { DrawableStringPair(it.first, it.second) }
 
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
+
 @Composable
 fun ColDisplay() {
     Column(modifier = Modifier.fillMaxSize()) {
         SearchBar()
         Spacer(modifier = Modifier.height(15.dp))
         AlignYourBodyRow()
+        FavoriteCollectionsGrid()
     }
 }
 
@@ -110,6 +124,7 @@ fun AlignYourBodyElement(
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(all = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             Image(
                 painter = painterResource(drawable),
@@ -122,7 +137,8 @@ fun AlignYourBodyElement(
 
             Text(
                 text = stringResource(id = text),
-                modifier = Modifier.padding(start = 10.dp)
+                modifier = Modifier.padding(start = 10.dp),
+                textAlign = TextAlign.Center,
             )
         }
     }
@@ -139,6 +155,25 @@ fun AlignYourBodyRow(
     ) {
 
         items(alignYourBodyData) { item ->
+            AlignYourBodyElement(item.drawable, item.text)
+        }
+    }
+}
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun FavoriteCollectionsGrid(
+    modifier: Modifier = Modifier
+) {
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ) {
+        items(favoriteCollectionsData) { item ->
             AlignYourBodyElement(item.drawable, item.text)
         }
     }
