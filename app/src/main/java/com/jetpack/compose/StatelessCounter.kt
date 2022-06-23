cc04bf1702c89ac68823bec8bd845e3e3b89d601
Juan Mendez <juan.mendez1@grainger.com>
2022-06-22 20:03:55 -0500

    02: learning about hoisting and stateful/stateless composables

---
+++ app/src/main/java/com/jetpack/compose/StatelessCounter.kt
@@ -0,0 +1,46 @@
+package com.jetpack.compose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, onClear: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            var showTask by rememberSaveable { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = {
+                        showTask = false
+                    },
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
+            Text("You've had $count glasses.")
+        }
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+            Button(onClick = { onIncrement() }, enabled = count < 10) {
+                Text("Add one")
+            }
+            if(count > 0) {
+                Button(onClick = { onClear() }) {
+                    Text("Clear")
+                }
+            }
+        }
+
+    }
+}
