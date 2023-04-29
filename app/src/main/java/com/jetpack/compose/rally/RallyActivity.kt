d6c96eec4e5fbbc14d2dfb91cc0d3fbe08f406fa
Juan Mendez <juan.mendez1@grainger.com>
2023-04-29 14:51:33 -0500

    05: refactoring

--- app/src/main/java/com/jetpack/compose/rally/RallyActivity.kt
+++ app/src/main/java/com/jetpack/compose/rally/RallyActivity.kt
@@ -19,22 +19,12 @@ package com.jetpack.compose.rally
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.jetpack.compose.rally.ui.accounts.AccountsScreen
-import com.jetpack.compose.rally.ui.accounts.SingleAccountScreen
-import com.jetpack.compose.rally.ui.bills.BillsScreen
 import com.jetpack.compose.rally.ui.components.RallyTabRow
-import com.jetpack.compose.rally.ui.overview.OverviewScreen
 import com.jetpack.compose.rally.ui.theme.RallyTheme
 
 /**
@@ -67,66 +57,7 @@ fun RallyApp() {
                 )
             }
         ) { innerPadding ->
-            NavHost(
-                navController = navController,
-                startDestination = Overview.route,
-                modifier = Modifier.padding(innerPadding)
-            ) {
-                // builder parameter will be defined here as the graph
-
-                composable(route = Overview.route) {
-                    OverviewScreen(
-                        onClickSeeAllAccounts = {
-                            navController.navigateSingleTopTo(Accounts.route)
-                        },
-                        onClickSeeAllBills = {
-                            navController.navigateSingleTopTo(Bills.route)
-                        },
-                        onAccountClick = { accountType ->
-                            navController.navigateSingleTopTo(
-                                SingleAccount.getRouteWithArgument(accountType)
-                            )
-                        }
-                    )
-                }
-
-                composable(route = Accounts.route) {
-                    AccountsScreen(
-                        onAccountClick = { accountType ->
-                            navController.navigateSingleTopTo(
-                                SingleAccount.getRouteWithArgument(accountType)
-                            )
-                        }
-                    )
-                }
-                composable(route = Bills.route) {
-                    BillsScreen()
-                }
-
-                composable(
-                    route = SingleAccount.routeWithArgs,
-                    arguments = SingleAccount.arguments,
-                    deepLinks = SingleAccount.deepLinks,
-                    ) { navBackStackEntry ->
-                    // Retrieve the passed argument
-                    val accountType = navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
-
-                    SingleAccountScreen(accountType)
-                }
-
-            }
+            RallyNavHost(navController, innerPadding)
         }
-    }
-}
-
-fun NavHostController.navigateSingleTopTo(route: String) {
-    navigate(route) {
-        popUpTo(
-            graph.findStartDestination().id
-        ) {
-            saveState = true
-        }
-        launchSingleTop = true
-        restoreState = true
     }
 }
---
+++ app/src/main/java/com/jetpack/compose/rally/RallyNavHost.kt
@@ -0,0 +1,78 @@
+package com.jetpack.compose.rally
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.jetpack.compose.rally.ui.accounts.AccountsScreen
+import com.jetpack.compose.rally.ui.accounts.SingleAccountScreen
+import com.jetpack.compose.rally.ui.bills.BillsScreen
+import com.jetpack.compose.rally.ui.overview.OverviewScreen
+
+@Composable
+fun RallyNavHost(navController: NavHostController, innerPadding: PaddingValues) {
+    NavHost(
+        navController = navController,
+        startDestination = Overview.route,
+        modifier = Modifier.padding(innerPadding)
+    ) {
+        // builder parameter will be defined here as the graph
+
+        composable(route = Overview.route) {
+            OverviewScreen(
+                onClickSeeAllAccounts = {
+                    navController.navigateSingleTopTo(Accounts.route)
+                },
+                onClickSeeAllBills = {
+                    navController.navigateSingleTopTo(Bills.route)
+                },
+                onAccountClick = { accountType ->
+                    navController.navigateSingleTopTo(
+                        SingleAccount.getRouteWithArgument(accountType)
+                    )
+                }
+            )
+        }
+
+        composable(route = Accounts.route) {
+            AccountsScreen(
+                onAccountClick = { accountType ->
+                    navController.navigateSingleTopTo(
+                        SingleAccount.getRouteWithArgument(accountType)
+                    )
+                }
+            )
+        }
+        composable(route = Bills.route) {
+            BillsScreen()
+        }
+
+        composable(
+            route = SingleAccount.routeWithArgs,
+            arguments = SingleAccount.arguments,
+            deepLinks = SingleAccount.deepLinks,
+        ) { navBackStackEntry ->
+            // Retrieve the passed argument
+            val accountType = navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
+
+            SingleAccountScreen(accountType)
+        }
+
+    }
+}
+
+fun NavHostController.navigateSingleTopTo(route: String) {
+    navigate(route) {
+        popUpTo(
+            graph.findStartDestination().id
+        ) {
+            saveState = true
+        }
+        launchSingleTop = true
+        restoreState = true
+    }
+}
