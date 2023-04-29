318add692221927083402b49d0c9ef817c006c85
Juan Mendez <juan.mendez1@grainger.com>
2023-04-29 14:17:46 -0500

    03: passing arguments

--- app/src/main/java/com/jetpack/compose/rally/RallyActivity.kt
+++ app/src/main/java/com/jetpack/compose/rally/RallyActivity.kt
@@ -26,11 +26,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.jetpack.compose.rally.ui.accounts.AccountsScreen
+import com.jetpack.compose.rally.ui.accounts.SingleAccountScreen
 import com.jetpack.compose.rally.ui.bills.BillsScreen
 import com.jetpack.compose.rally.ui.components.RallyTabRow
 import com.jetpack.compose.rally.ui.overview.OverviewScreen
@@ -80,16 +83,38 @@ fun RallyApp() {
                         },
                         onClickSeeAllBills = {
                             navController.navigateSingleTopTo(Bills.route)
+                        },
+                        onAccountClick = { accountType ->
+                            navController.navigateSingleTopTo(
+                                SingleAccount.getRouteWithArgument(accountType)
+                            )
                         }
                     )
                 }
 
                 composable(route = Accounts.route) {
-                    AccountsScreen()
+                    AccountsScreen(
+                        onAccountClick = {accountType ->
+                            navController.navigateSingleTopTo(
+                                SingleAccount.getRouteWithArgument(accountType)
+                            )
+                        }
+                    )
                 }
                 composable(route = Bills.route) {
                     BillsScreen()
                 }
+
+                composable(
+                    route = SingleAccount.routeWithArgs,
+                    arguments = SingleAccount.arguments
+                ) { navBackStackEntry ->
+                    // Retrieve the passed argument
+                    val accountType = navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
+
+                    SingleAccountScreen(accountType)
+                }
+
             }
         }
     }
@@ -106,4 +131,3 @@ fun NavHostController.navigateSingleTopTo(route: String) {
         restoreState = true
     }
 }
-
